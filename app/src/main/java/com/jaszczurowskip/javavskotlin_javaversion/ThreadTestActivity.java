4b54feb353322a62963b8df29c12257ee8bb1117
JaszczurowskiP <P.Jaszczurowski@tt.com.pl>
2019-03-22 15:19:23 +0100

    Prepared view for threading in Thread View

--- app/src/main/java/com/jaszczurowskip/javavskotlin_javaversion/ThreadTestActivity.java
+++ app/src/main/java/com/jaszczurowskip/javavskotlin_javaversion/ThreadTestActivity.java
@@ -1,42 +1,89 @@
 package com.jaszczurowskip.javavskotlin_javaversion;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ProgressBar;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class ThreadTestActivity extends AppCompatActivity {
-    private TextView tvFirstThread;
     private Button bttnStart;
-    private ProgressBar bar;
+    private Spinner spinnerTransdormationMode;
+    private Spinner spinnerThreadMode;
+    private ImageView imgFirstPhoto;
+    private ImageView imgSecondPhoto;
+    private ImageView imgThirdPhoto;
+    private ImageView imgFourthPhoto;
+    private ImageView imgFifthPhoto;
+    private ImageView imgSixthPhoto;
+    private TextView tvFirstThread;
+    private TextView tvSecondThread;
+    private TextView tvThirdThread;
+    private TextView tvFourthThread;
+    private TextView tvFifthThread;
+    private TextView tvSixthThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thread_test);
-        tvFirstThread = findViewById(R.id.tvFirstThread);
         bttnStart = findViewById(R.id.bttnStart);
-        bar =  findViewById(R.id.threadProgress);
+        imgFirstPhoto = findViewById(R.id.imgFirstPhoto);
+        imgSecondPhoto = findViewById(R.id.imgSecondPhoto);
+        imgThirdPhoto = findViewById(R.id.imgThirdPhoto);
+        imgFourthPhoto = findViewById(R.id.imgFourthPhoto);
+        imgFifthPhoto = findViewById(R.id.imgFifthPhoto);
+        imgSixthPhoto = findViewById(R.id.imgSixthPhoto);
+        tvFirstThread = findViewById(R.id.tvFirstPhoto);
+        tvSecondThread = findViewById(R.id.tvSecondPhoto);
+        tvThirdThread = findViewById(R.id.tvThirdPhoto);
+        tvFourthThread = findViewById(R.id.tvFourthPhoto);
+        tvFifthThread = findViewById(R.id.tvFifthPhoto);
+        tvSixthThread = findViewById(R.id.tvSixthPhoto);
+        spinnerTransdormationMode = findViewById(R.id.spinnerTransformationMode);
+        spinnerThreadMode = findViewById(R.id.spinnerThreadMode);
         registerButtons();
+        setupSpinner(spinnerTransdormationMode, R.array.transformations_mode);
+        setupSpinner(spinnerThreadMode, R.array.thread_mode);
     }
+
+    private void setupSpinner(Spinner spinner, int array) {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
 
     private void registerButtons() {
         bttnStart.setOnClickListener(v -> {
-            bar.setProgress(0);
-            //tvFirstThread.setText("");
+            inversePhotoColors(imgFirstPhoto);
+            inversePhotoColors(imgSecondPhoto);
+            inversePhotoColors(imgThirdPhoto);
+            inversePhotoColors(imgFourthPhoto);
+            inversePhotoColors(imgFifthPhoto);
+            inversePhotoColors(imgSixthPhoto);
         });
     }
 
-    private static void spin(int milliseconds) {
-        long sleepTime = milliseconds*1000000L; // convert to nanoseconds
-        long startTime = System.nanoTime();
-        while ((System.nanoTime() - startTime) < sleepTime) {}
+    private void inversePhotoColors(ImageView img) {
+        Bitmap bitmap = ((BitmapDrawable) img.getDrawable()).getBitmap();
+        int length = bitmap.getWidth() * bitmap.getHeight();
+        int[] array = new int[length];
+        Bitmap mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+        mutableBitmap.getPixels(array, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        for (int i = 0; i < length; i++) {
+            array[i] = invertRGB(array[i]);
+        }
+        mutableBitmap.setPixels(array, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        img.setImageBitmap(mutableBitmap);
+    }
+
+    private int invertRGB(int rgb) {
+        return ((~rgb) & 0x00FFFFFF) | 0xFF000000;
     }
 }
